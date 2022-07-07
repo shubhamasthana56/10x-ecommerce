65da050e7770707746cd4422b1be2fdd1da00a47
shubham asthana <shubhamasthana56@gmail.com>
2022-07-07 21:32:59 +0530

    client changes and signup addition

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import Signup from "./components/signup/signup";
+const App = ()=> {
+  return (
+    <>
+      Ecommerce App works
+      <Signup/>
+    </>
+  )
+}
+export default App;
---
+++ client/src/components/signup/signup.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import axios from "axios";
+const Signup = ()=> {
+    const [signupState, setSignupState] = useState({});
+    const signUpFormData = [{attr: "username", type: "text", id: "username", label: "Username:"},
+                            {attr: "phone_number", type: "text", id: "phone_number", label: "Mobile Number:"},
+                            {attr: "password", type: "password", id: "password", label: "Password:"}]
+    const handleUserAdd = ()=> {
+        console.log(signupState);
+        axios({
+            url: "http://localhost:3001/user/signup",
+            method: "POST",
+            headers: {
+            },
+            data: signupState
+        }).then((res)=> {
+            console.log(res)
+        }).catch((err)=> {
+            console.log(err)
+        })
+    }
+    const handleInputChange = (e, id)=> {
+        setSignupState({...signupState, [id]: e.target.value})
+    }
+    return (
+        <>
+            <div>
+                <form>
+                    {signUpFormData.map((formKey)=> {
+                        return (
+                            <div>
+                                <div>
+                                    <label for={formKey.id}>{formKey.label}</label>
+                                </div>
+                                <div>
+                                    <input type={formKey.type} id={formKey.id} onChange={(e)=> {handleInputChange(e,formKey.id)}}/>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </form>
+                <button type="button" onClick={handleUserAdd}>Submit</button>
+            </div>
+        </>
+    )
+}
+export default Signup;
